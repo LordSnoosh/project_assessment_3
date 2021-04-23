fae7b678fb845eb4e7e56a295474fdccd2dad942
github_username <lordsnoosh@gmail.com>
2021-04-23 11:44:07 -0700

    2nd migrate

---
+++ main_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+class Wish(models.Model):
+    item = models.TextField(max_length=120)
+    def __str__(self):
+        return self.item
+    def get_absolute_url(self):
+        return reverse('index')
